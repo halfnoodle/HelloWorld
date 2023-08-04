f26acfef124dee7ae4ac34e8651e1c92091c2802
halfnoodle <hunguyen9@yahoo.com>
2023-08-03 19:00:03 -0700

    Update ScriptBuild.bat

--- ScriptBuild.bat
+++ ScriptBuild.bat
@@ -4,5 +4,6 @@ set msBuildDir=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\C
 
 REM rm bin\Debug\*.*
 REM
+REM
 call "%msBuildDir%\MSbuild.exe" HelloWorld.sln /t:Clean,Build /p:toolsversion=4.8 /p:langversion=7.3 /p:Configuration=Debug /p:Platform="Any CPU" -m /l:FileLogger,Microsoft.Build.Engine;logfile=Manual_MSBuild_ReleaseVersion_LOG.log
 set msBuildDir=
